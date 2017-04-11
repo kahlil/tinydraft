e58867b60259c1795356d0f10c3a697a821bb959
Kahlil Lechelt <hello@kahlil.info>
2017-04-11 14:18:59 +0200

    move global styles to the right place, remove hashes from built file names

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [
-    '../../node_modules/normalize-css/normalize.css',
-    '../../node_modules/basscss/css/basscss.min.css',
-    './app.component.css',
-  ],
+  styleUrls: ['./app.component.css'],
   encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
