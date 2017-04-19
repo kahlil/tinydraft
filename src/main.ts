812090b61dd94068696b42f7782c7e986266ec3a
Kahlil Lechelt <hello@kahlil.info>
2017-04-19 10:35:28 +0200

    use fastclick to remove click delay

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import attachFastClick from 'fastclick';
+
+attachFastClick(document.body);
+
 if (environment.production) {
   enableProdMode();
 }
